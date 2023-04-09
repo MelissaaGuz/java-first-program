506ab32818dc49c891981a21ceaea21872e0162b
MelissaaGuz <melissaguzman1210@gmail.com>
2023-04-09 02:51:49 -0400

    1st

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,23 @@
-package com.h2;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 
-    public static int doubleTheNumber(int number) {
-        return -1;
+    public static int doubleTheNumber(int num) {
+        return 2 * num;
+    }
+
+    private static int add(int[] num) {
+        var sum = 0;
+        for(int number: num) {
+            sum += num;
+        }
+        return sum;
     }
 }
