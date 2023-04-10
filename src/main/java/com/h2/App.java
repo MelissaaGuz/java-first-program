64596b7df633804cbb460364eb150c22c0cc3e3e
MelissaaGuz <melissaguzman1210@gmail.com>
2023-04-09 20:25:57 -0400

    1st commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,25 +1,44 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.text.DecimalFormat;
+
+public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate){
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
     }
 
-    public static int doubleTheNumber(int number) {
-
-        return 2 * number;
+    private int getNumberOfPayments(){
+        return termInYears*12;
     }
-    private static int add(int[] numbers) {
-        var sum = 0;
-        for(int number: numbers) {
-            sum += number;
-        }
-        return sum;
+    private float getMonthlyInterestRate(){
+        float interestRate = annualRate/100;
+        interestRate /= 12;
+        return interestRate;
+    }
+    public void calculateMonthlyPayment(){
+        long P = loanAmount;
+        float r = getMonthlyInterestRate();
+        int n = getNumberOfPayments();
+        double M = P * (((r * Math.pow(1+r,n))) / ((Math.pow((1+r),n)) - 1));
+        this.monthlyPayment = M;
+    }
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
+    public static void main(String[] args){
+        long loanAmount = Utilities.getLongValue(args[0]);
+        int termInYears = Utilities.getIntValue(args[1]);
+        float annualRate = Utilities.getFloatValue(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
     }
 }
